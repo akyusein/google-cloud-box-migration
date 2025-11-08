7f696bcf739cdd160f8626f4a39fb25d91e2e40b
Aksel Yusein <akselyusein@Aksels-MacBook-Pro.local>
2025-11-08 03:05:32 +0000

    GCS-05

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from google_storage_ops import GoogleCloudStorage
 from utils import get_client
 
 try:
+    #Initializes the Google Cloud Storage object
     operation = GoogleCloudStorage('bucket', "prefix", get_client(), "collected_blobs.csv")
 
+    #Collects the blobs by performing API calls and storing the data in a cache.
     operation.collect_blobs()
 
+    #The method returns the sum of the root prefixes from the cached data.
     operation.format_prefix()
 
     print("Operations completed successfully!")
